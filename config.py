eff96616ba73dfe85b7b5253d6db453d2c1cd9ce
Mango <83092179+ThermalMango@users.noreply.github.com>
2026-04-30 17:34:44 -0600

    Fixed API issues, code is back to working normally. Cleaned up some code for sending the requests. Cleaned up code for random duration and intensity.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Change these variables:
 username = "" # Your username on the PiShock website
 api_key = "" # Your api key from the PiShock website
-share_code = [""] # List all share codes for each shocker you want to use. Input as ["<code1>","<code2>",...]
-app_name = "" # What you want to name the app
+shocker_ids = ["",""] # List all share codes for each shocker you want to use. Input as ["<code1>","<code2>",...]
+app_name = "PishockPy/0.0.5" # What you want to name the app
 test_mode = False  # Set to 'True' if you want to bypass setting an alarm
-snooze_duration =  # Set in minutes how long you want a snooze. If you don't want a snooze, set to '0'
+snooze_duration = 0 # Set in minutes how long you want a snooze. If you don't want a snooze, set to '0'
